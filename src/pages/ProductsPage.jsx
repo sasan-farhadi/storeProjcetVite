f80a1eda3732b7456bc97478149e05518ce3f176
Sasan <sasanfarhadi@gmail.com>
2024-09-17 23:41:59 +0330

    create search input

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,17 +1,35 @@
+import { useState } from 'react'
 import Card from '../components/Card'
 import Loader from "../components/Loader"
 import { useProducts } from '../context/ProductContext'
 import styles from './ProductsPage.module.css'
+
+import { ImSearch } from 'react-icons/im'
 const ProductsPage = () => {
     const products = useProducts()
+    const [search, setSearch] = useState("")
+
+    const searchHandler = () => {
+        console.log("first")
+    }
+
     return (
-        <div className={styles.container}>
-            <div className={styles.products}>
-                {!products.length && <Loader />}
-                {products.map(p => (<Card key={p.id} data={p} />))}
+        <>
+            <div>
+                <input type="text"
+                    placeholder='Search ...'
+                    value={search}
+                    onChange={e => setSearch(e.target.value.toLowerCase().trimStart())} />
+                <button onClick={searchHandler}><ImSearch /></button>
             </div>
-            <div>SideBar</div>
-        </div>
+            <div className={styles.container}>
+                <div className={styles.products}>
+                    {!products.length && <Loader />}
+                    {products.map(p => (<Card key={p.id} data={p} />))}
+                </div>
+                <div>SideBar</div>
+            </div>
+        </>
     )
 }
 
