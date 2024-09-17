95d30d5dc421cb2d5072ff6a08a129476eda184c
Sasan <sasanfarhadi@gmail.com>
2024-09-17 23:56:04 +0330

    category

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -5,6 +5,7 @@ import { useProducts } from '../context/ProductContext'
 import styles from './ProductsPage.module.css'
 
 import { ImSearch } from 'react-icons/im'
+import { FaListUl } from 'react-icons/fa'
 const ProductsPage = () => {
     const products = useProducts()
     const [search, setSearch] = useState("")
@@ -13,6 +14,12 @@ const ProductsPage = () => {
         console.log("first")
     }
 
+    const categoryHandler = (e) => {
+        const { tagName } = e.target
+        const category = event.target.innerText.toLowerCase()
+        if (tagName !== "LI") return
+        console.log(category)
+    }
     return (
         <>
             <div>
@@ -27,7 +34,19 @@ const ProductsPage = () => {
                     {!products.length && <Loader />}
                     {products.map(p => (<Card key={p.id} data={p} />))}
                 </div>
-                <div>SideBar</div>
+                <div>
+                    <div>
+                        <FaListUl />
+                        <p></p>
+                    </div>
+                    <ul onClick={categoryHandler}>
+                        <li>All</li>
+                        <li>Electronice</li>
+                        <li>Jewelery</li>
+                        <li>Men's Clothing</li>
+                        <li>Women's Clothing</li>
+                    </ul>
+                </div>
             </div>
         </>
     )
