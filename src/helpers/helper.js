06cab043f4d3666096c5557f06f9259de854b6bb
Sasan <sasanfarhadi@gmail.com>
2024-09-26 21:17:33 +0330

    search and category handler

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -10,10 +10,32 @@ const searchProducts = (products, search) => {
     return searchedProducts
 }
 
-
 const filterProducts = (products, category) => {
     if (!category) return products
     const filterdProducts = products.filter(p => p.category == category)
     return filterdProducts
 }
-export { shortenText, searchProducts, filterProducts }
+
+const createQueryObject = (currentQuery, newQuery) => {
+    if (newQuery.category == "all") {
+        const { category, ...rest } = currentQuery
+        return rest
+    }
+    if (newQuery.search == "") {
+        const { search, ...rest } = currentQuery
+        return rest
+    }
+
+    return { ...currentQuery, ...newQuery }
+}
+
+const getInitialQuery = (searchParams) => {
+    const query = {}
+    const category = searchParams.get("category")
+    const search = searchParams.get("search")
+    if (category) query.category = category
+    if (search) query.search = search
+    return query
+}
+
+export { shortenText, searchProducts, filterProducts, createQueryObject, getInitialQuery }
--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,70 +1,46 @@
 import { useEffect, useState } from 'react'
+import { useSearchParams } from 'react-router-dom'
 import Card from '../components/Card'
 import Loader from "../components/Loader"
 import { useProducts } from '../context/ProductContext'
 import styles from './ProductsPage.module.css'
 
-import { ImSearch } from 'react-icons/im'
-import { FaListUl } from 'react-icons/fa'
-import { filterProducts, searchProducts } from '../helpers/helper'
+import { filterProducts, getInitialQuery, searchProducts } from '../helpers/helper'
+import SearchBox from '../components/SearchBox'
+import SideBar from '../components/SideBar'
+
 const ProductsPage = () => {
     const products = useProducts()
-
     const [displyed, setDisplayed] = useState([])
     const [search, setSearch] = useState("")
     const [query, setQuery] = useState({})
 
+    const [searchParams, setSearchParams] = useSearchParams()
+
     useEffect(() => {
         setDisplayed(products)
+        setQuery(getInitialQuery(searchParams))
     }, [products])
 
 
     useEffect(() => {
+        setSearchParams(query)
+        setSearch(query.search || "")
         let finalProducts = searchProducts(products, query.search)
         finalProducts = filterProducts(finalProducts, query.category)
 
         setDisplayed(finalProducts)
     }, [query])
 
-    const searchHandler = () => {
-        setQuery(query => ({ ...query, search }))
-    }
-
-    const categoryHandler = (e) => {
-        const { tagName } = e.target
-        const category = e.target.innerText.toLowerCase()
-
-        if (tagName !== "LI") return
-        setQuery(query => ({ ...query, category }))
-    }
-
     return (
         <>
-            <div>
-                <input type="text"
-                    placeholder='Search ...'
-                    value={search}
-                    onChange={e => setSearch(e.target.value.toLowerCase().trimStart())} />
-                <button onClick={searchHandler}><ImSearch /></button>
-            </div>
+            <SearchBox search={search} setSearch={setSearch} setQuery={setQuery} />
             <div className={styles.container}>
                 <div className={styles.products}>
                     {!displyed.length && <Loader />}
                     {displyed.map(p => (<Card key={p.id} data={p} />))}
                 </div>
-                <div>
-                    <div>
-                        <FaListUl />
-                        <p>Categories</p>
-                    </div>
-                    <ul onClick={categoryHandler}>
-                        <li>All</li>
-                        <li>Electronice</li>
-                        <li>Jewelery</li>
-                        <li>Men's Clothing</li>
-                        <li>Women's Clothing</li>
-                    </ul>
-                </div>
+                <SideBar setQuery={setQuery} />
             </div>
         </>
     )
