c098bb9035d62ae068e857127395920f0fe198cc
Sasan <sasanfarhadi@gmail.com>
2024-09-27 23:45:33 +0330

    complate

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,7 +1,31 @@
+import BasketCard from '../components/BasketCard'
+import BasketSidebar from '../components/BasketSidebar'
+import { useCart } from '../context/CartContext'
+import styles from './CheckoutPage.module.css'
+
 const CheckoutPage = () => {
+    const [state, dispatch] = useCart()
+    const clickHandler = (type, payload) => dispatch({ type, payload })
+
+    if (!state.itemsCounter) {
+        return (
+            <div className={styles.container}>
+                <p>EMPTY</p>
+            </div>
+        )
+    }
+
     return (
-        <div>
-            check
+        <div className={styles.container}>
+            <BasketSidebar state={state} clickHandler={clickHandler} />
+            <div className={styles.products}>
+                {state.selectedItems.map(product => (
+                    <BasketCard
+                        key={product.id}
+                        data={product}
+                        clickHandler={clickHandler} />
+                ))}
+            </div>
         </div>
     )
 }
